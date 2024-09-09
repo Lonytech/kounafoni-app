b533e4738a0516d7a8a96cd356ff96c08360d258
Bouba <abubakrtraore@gmail.com>
2024-09-09 23:40:29 +0200

    final schedule for malijet articles scrap on github actions

--- src/malijet_news_updater.py
+++ src/malijet_news_updater.py
@@ -1,22 +1,15 @@
-from datetime import date
-
-import dateparser
+from datetime import date, timedelta
 
 from scraper import MaliJetDataScraper, ScrapDate
 
 if __name__ == "__main__":
 
     # Set the range to [yesterday - today] to not forget any article
-    # today_date = date.today()
-    # yesterday_date = today_date - timedelta(days=1)
-    begin_date = "2024-01-01"
-    end_date = "2024-09-09"
-
-    begin_date = dateparser.parse(begin_date).date()
-    end_date = dateparser.parse(end_date).date()
+    today_date = date.today()
+    yesterday_date = today_date - timedelta(days=1)
 
     scraper = MaliJetDataScraper(
-        date_range=ScrapDate(end_date=end_date, begin_date=begin_date)
+        date_range=ScrapDate(end_date=today_date, begin_date=yesterday_date)
     )
 
     # get all articles "A la Une" from Malijet in the date range given and save them
