e8e538a6b10a91ea5c96ed6ec2bb914ee566545a
Bouba <abubakrtraore@gmail.com>
2024-09-22 07:31:49 +0200

    compression worked, all gcs files in volume are compressed, will handle all of them

--- src/rag.py
+++ src/rag.py
@@ -80,9 +80,7 @@ class LocalRag:
                 file_path.as_posix(),
                 glob="**/*.csv",
                 loader_cls=CSVLoader,
-                # loader_kwargs={
-                #     "csv_args": {"delimiter": "\t", "autodetect_encoding": True}
-                # },
+                loader_kwargs={"csv_args": {"delimiter": "\t", "compression": "gzip"}},
             )
         else:
             loader = CSVLoader(file_path=file_path, csv_args={"delimiter": "\t"})
