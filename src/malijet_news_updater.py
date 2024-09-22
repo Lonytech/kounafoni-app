2ce4bc28ab2da43ee3d9804e538c46f78620ecb7
Bouba <abubakrtraore@gmail.com>
2024-09-22 14:00:44 +0200

    reswitch back to delta only 1 day

--- src/malijet_news_updater.py
+++ src/malijet_news_updater.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
 
     # Set the range to [yesterday - today] to not forget any article
     today_date = date.today()
-    yesterday_date = today_date - timedelta(days=265)
+    yesterday_date = today_date - timedelta(days=1)
 
     scraper = MaliJetDataScraper(
         date_range=ScrapDate(end_date=today_date, begin_date=yesterday_date)
