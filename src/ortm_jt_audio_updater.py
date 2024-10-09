49e2c49962a6dc51b858bd8f693de6bc2025939f
Bouba <abubakrtraore@gmail.com>
2024-10-09 21:08:25 +0200

    changing website

--- src/ortm_jt_audio_updater.py
+++ src/ortm_jt_audio_updater.py
@@ -2,6 +2,8 @@ import logging
 from datetime import datetime, timedelta
 from pathlib import Path
 
+import yt_dlp
+
 from speech_to_text import AdvancedYouTubeDownloader
 
 # from speech_to_text import TVNewsSpeechToText
@@ -16,15 +18,51 @@ SUMMARIZED_TEXTS_PATH = STT_PATH / "summarized_texts"
 JT_20H_PLAYLIST_URL = "https://youtube.com/playlist?list=PLDBQmURq6pOfBKc6WU0wXTg2vxAjxjQel&si=n9iNX7AUi-SpNN_N"
 
 
-if __name__ == "__main__":
+def download_audio_from_invidious(video_id: str, output_path: str = "."):
+    """
+    Télécharge l'audio d'une vidéo à partir de l'instance Invidious en utilisant l'identifiant YouTube de la vidéo.
 
-    downloader = AdvancedYouTubeDownloader()
-    url = "https://www.youtube.com/watch?v=wy7vxd61uwg"
-    result = downloader.download_audio(url)
-    if result:
-        logger.info(f"Fichier téléchargé avec succès: {result}")
-    else:
-        logger.error("Échec du téléchargement")
+    :param video_id: L'identifiant de la vidéo YouTube (ex: 'abc123')
+    :param output_path: Le chemin où le fichier audio sera enregistré.
+    :return: Chemin du fichier téléchargé.
+    """
+    # Créer l'URL Invidious à partir de l'ID de la vidéo YouTube
+    invidious_url = f"https://yewtu.be/watch?v={video_id}"
+
+    # Options de yt-dlp pour télécharger uniquement l'audio
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": f"{output_path}/%(title)s.%(ext)s",
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "192",
+            }
+        ],
+        "quiet": True,  # Garde le log silencieux
+        "no_warnings": True,
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(invidious_url, download=True)
+            print(f"Audio téléchargé : {info_dict['title']}")
+            return f"{output_path}/{info_dict['title']}.mp3"
+    except Exception as e:
+        print(f"Erreur lors du téléchargement: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    download_audio_from_invidious(video_id="wy7vxd61uwg")
+    # downloader = AdvancedYouTubeDownloader()
+    # url = "https://www.yewtu.be/watch?v=wy7vxd61uwg"
+    # result = downloader.download_audio(url)
+    # if result:
+    #     logger.info(f"Fichier téléchargé avec succès: {result}")
+    # else:
+    #     logger.error("Échec du téléchargement")
 
 
 # if __name__ == "__main__":
