55785af04704111f61705fa1c6f4693060fff6ba
Bouba <abubakrtraore@gmail.com>
2024-11-17 20:17:38 +0100

    first attempt to read audio files from gcs

--- src/app/app.py
+++ src/app/app.py
@@ -1,91 +1,39 @@
 import os
+from collections import defaultdict
+from pathlib import Path
 from typing import Any
 
-from flask import Flask, Response, redirect, render_template
+from flask import Flask, Response, redirect, render_template, send_from_directory
+
+AUDIOS_DIRECTORY_PATH = Path(__file__).parents[2] / "data" / "piper" / "exports"
+
+
+if os.environ.get("CHATBOT_ENV") == "production":
+    AUDIOS_DIRECTORY_PATH = (
+        Path(__file__).parents[2] / "external_volume" / "data" / "exports"
+    )
 
 app = Flask(__name__)
 
-# Test audio data
-audios = {
-    "2024-05-14": [
-        {
-            "date": "2024-05-14",
-            "media": "ORTM",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-14.mp3",
-        },
-        {
-            "date": "2024-05-14",
-            "media": "Malijet",
-            "duration": "4:20",
-            "file": "Résumé ORTM - 2024-05-14.mp3",
-        },
-        {
-            "date": "2024-05-14",
-            "media": "Bamada",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-14.mp3",
-        },
-        {
-            "date": "2024-05-14",
-            "media": "aBamako",
-            "duration": "4:20",
-            "file": "Résumé ORTM - 2024-05-14.mp3",
-        },
-    ],
-    "2024-05-24": [
-        {
-            "date": "2024-05-24",
-            "media": "ORTM",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-24.mp3",
-        },
-        {
-            "date": "2024-05-24",
-            "media": "Malijet",
-            "duration": "4:20",
-            "file": "Résumé ORTM - 2024-05-24.mp3",
-        },
-        {
-            "date": "2024-05-24",
-            "media": "Bamada",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-24.mp3",
-        },
-        {
-            "date": "2024-05-24",
-            "media": "aBamako",
-            "duration": "4:20",
-            "file": "Résumé ORTM - 2024-05-24.mp3",
-        },
-    ],
-    "2024-05-16": [
-        {
-            "date": "2024-05-16",
-            "media": "ORTM",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-16.mp3",
-        },
-        {
-            "date": "2024-05-16",
-            "media": "Malijet",
-            "duration": "4:20",
-            "file": "Résumé Malijet - 2024-05-16.mp3",
-        },
-        {
-            "date": "2024-05-16",
-            "media": "Bamada",
-            "duration": "3:45",
-            "file": "Résumé ORTM - 2024-05-16.mp3",
-        },
-        {
-            "date": "2024-05-16",
-            "media": "aBamako",
-            "duration": "4:20",
-            "file": "Résumé ORTM - 2024-05-16.mp3",
-        },
-    ],
-}
+# get a complete list of audio from directory
+audio_dates_dirs = [d for d in AUDIOS_DIRECTORY_PATH.iterdir() if d.is_dir()]
+audios = defaultdict(list)
+for folder in audio_dates_dirs:
+    for f in folder.iterdir():
+        print(f)
+        if f.is_file():
+            print(f)
+            audios[folder.name].append(
+                {
+                    "media": "ORTM",
+                    "file": f.name,
+                }
+            )
+
+
+@app.route("/data/<path:filename>")
+def serve_audio(filename: str) -> Any:
+    return send_from_directory(AUDIOS_DIRECTORY_PATH, filename)
 
 
 @app.route("/")
@@ -108,5 +56,5 @@ def chat() -> Any:
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
