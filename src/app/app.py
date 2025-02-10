4abda1724eb6ae91193c962c038187aec2750b18
Bouba <abubakrtraore@gmail.com>
2025-02-10 14:38:08 +0100

    turn off debug

--- src/app/app.py
+++ src/app/app.py
@@ -55,5 +55,5 @@ def chat() -> Any:
 
 
 if __name__ == "__main__":
-    # app.run(host="0.0.0.0", port=5000)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
